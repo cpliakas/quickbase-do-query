635c4319af7fe867dcf1a62d48e1ca4740fb2538
cpliakas <cpliakas@gmail.com>
2019-02-23 22:31:10 -0500

    Added qbiface to enable mocking

---
+++ qbiface/interface.go
@@ -0,0 +1,18 @@
+package qbiface
+
+import (
+	qb "github.com/cpliakas/quickbase-do-query/quickbase"
+)
+
+// ClientAPI provides an interface to enable mocking the Quick Base service
+// client's API calls.
+type ClientAPI interface {
+	AddRecord(*qb.AddRecordInput) (qb.AddRecordOutput, error)
+	Authenticate(*qb.AuthenticateInput) (qb.AuthenticateOutput, error)
+	DoQuery(*qb.DoQueryInput) (qb.DoQueryOutput, error)
+	EditRecord(*qb.EditRecordInput) (qb.EditRecordOutput, error)
+	GetSchema(*qb.GetSchemaInput) (qb.GetSchemaOutput, error)
+	ImportFromCSV(*qb.ImportFromCSVInput) (qb.ImportFromCSVOutput, error)
+	SetVariable(*qb.SetVariableInput) (qb.SetVariableOutput, error)
+	UploadFile(*qb.UploadFileInput) (qb.UploadFileOutput, error)
+}
